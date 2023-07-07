d6d38d094c2e2bf287fd558d5fdd6b61f8c1a69f
Taro Sato <okomestudio@gmail.com>
2023-07-07 09:58:44 -0700

    Add documentation

--- src/oreilly_annotations_exporter/__main__.py
+++ src/oreilly_annotations_exporter/__main__.py
@@ -1,3 +1,10 @@
+"""Export annotations from O'Reilly Learning API.
+
+The command fetches JSON responses including annotations from the O'Reilly Learning API.
+It then parses annotation metadata to reconstruct their placement in the original XML
+document. The built-in exporter then parses the reconstructed XML to send generate
+annotation export in the desired format to the standard output stream.
+"""
 from argparse import ArgumentParser
 from pathlib import Path
 
@@ -5,16 +12,29 @@ from .drivers import entry_point
 
 
 def main():
-    p = ArgumentParser()
-    p.add_argument("json_dump", type=Path, help="JSON dump of API responses")
+    p = ArgumentParser(description=__doc__)
+    p.add_argument(
+        "json_dump",
+        type=Path,
+        help="JSON API response dump; will be created if not exist",
+    )
     p.add_argument(
         "--cookies",
         type=str,
         default="cookies",
-        help="Cookies string or file containing it",
+        help="cookies string or file containing it; obtain this from API request",
     )
-    p.add_argument("--export", type=str, default="csv")
-    p.add_argument("--epub-id", action="append")
+    p.add_argument(
+        "--export",
+        type=str,
+        default="csv",
+        help="export format, `csv', `raw_xml', or plugin module name",
+    )
+    p.add_argument(
+        "--epub-id",
+        action="append",
+        help="epub ID/ISBN13 without hyphens for filtering output",
+    )
 
     args = p.parse_args()
 
