9b40d7db5d629c9b47ebaf6ebda585d57ce28a7f
Taro Sato <okomestudio@gmail.com>
2023-07-07 01:47:04 -0700

    Update multiline string handling

--- plugins/org.py
+++ plugins/org.py
@@ -1,3 +1,11 @@
+import re
+
+
+def _process_spacing(s):
+    s = re.sub(r"\n[ \t\r\f\v]+", " ", s)  # remove line breaks
+    return s
+
+
 def export(parsed):
     output = []
     for tree in parsed.values():
@@ -7,14 +15,25 @@ def export(parsed):
             output.append(f"** { chapter.get('title') }")
             output.append("")
             for annotation in chapter.iter("annotation"):
-                quote = " ".join(
-                    x if x else "\n" for x in annotation.get("quote").split("\n")
-                )
+                quote = _process_spacing(annotation.get("quote"))
+
+                text = annotation.get("text")
+                if text:
+                    m = re.match(r"^[hH](\d+)[^\d]*", text)
+                    if m:
+                        heading_level = int(m.group(1))
+                        output.append("*" * (heading_level + 1) + " " + quote)
+                        output.append("")
+                        continue
 
                 output.append(f"#+begin_quote")
                 output.append(quote)
                 output.append(f"#+end_quote")
                 output.append("")
+                if text:
+                    output.append(text)
+                    output.append("")
+
         output.append("")
 
     print("\n".join(output))
